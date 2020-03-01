0fb9488ade66fa7a9323ed487c30322ad3c376e9
Gideon Mills <GydeonMylls@users.noreply.github.com>
2020-03-01 23:10:51 +0000

    Fix Copyright

--- README.md
+++ README.md
@@ -11,5 +11,7 @@ Periodically tweets warnings about locations with high CO₂ concentrations.
     - URL of summary file
     - Switch to run as cronjob, i.e. cron manages repeating
         - Need to save state between invocations, may also be useful in continuous mode for seamless restarts
+    - Switch to set logging level
 - Cycle through locations over some period, rather than sending a barrage of tweets at once
 - Some sort of combining metric to represent stuffiness/quality of work environment
+- Occasional room recommendations
--- Tweeter.py
+++ Tweeter.py
@@ -1,6 +1,4 @@
-#  MIT License
-#
-#  Copyright (c) 2020 GP2020-Sierra
+#  Copyright (c) 2020 University of Cambridge Computer Laboratory Group Projects 2020 Team Sierra
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
@@ -20,29 +18,7 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
-#  MIT License
-#
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#
 import datetime
-#  MIT License
-#
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#
 import json
 import logging
 import os
